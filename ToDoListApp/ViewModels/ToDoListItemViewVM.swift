97003f751e76821c010d9e9f59e3c323d4203d61
Trance <xp46290@gmail.com>
2023-05-13 09:54:47 -0400

    Allow setting and saving of checked off items

--- ToDoListApp/ViewModels/ToDoListItemViewVM.swift
+++ ToDoListApp/ViewModels/ToDoListItemViewVM.swift
@@ -6,6 +6,8 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 /// ViewModel for single to do list item view (each row in items list)
 
@@ -13,6 +15,19 @@ class ToDoListItemViewVM: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
         
     }
 }
